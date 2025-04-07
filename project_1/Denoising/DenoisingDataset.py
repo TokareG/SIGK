0c8ffec144c7b3ef219376815b05f35966d51833
TokareG <j.tkrsky@gmail.com>
2025-04-07 13:26:01 +0200

    denoising fix

--- project_1/Denoising/DenoisingDataset.py
+++ project_1/Denoising/DenoisingDataset.py
@@ -17,10 +17,10 @@ class DenoisingDataset(Dataset):
         img_path = self.img_list[idx]
         input_path = os.path.join(self.input_dir, img_path)
         input_img = read_image(input_path).float() / 255.
-        real_image = copy.deepcopy(input_img)
+        target_image = copy.deepcopy(input_img)
         if self.transform:
             input_img = self.transform(input_img)
 
-        return input_img, real_image
+        return input_img, target_image
 
 
